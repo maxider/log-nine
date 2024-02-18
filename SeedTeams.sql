6ceac2ec6985b45a0e1a21c05dda119c3c303d4c
maxider <maxweilandt1998@gmail.com>
2024-02-18 19:45:45 +0100

    Added seed file for default Teams

---
+++ SeedTeams.sql
@@ -0,0 +1,22 @@
+INSERT INTO
+    Teams (Name, BoardId, SrFrequency, LrFrequency)
+VALUES
+    ("Yellow", 1, 116, 41),
+    ("Yellow 1", 1, 111, 41),
+    ("Yellow 2", 1, 112, 41),
+    ("Yellow 3", 1, 113, 41),
+    ("Yellow 4", 1, 114, 41),
+    ("Blue", 1, 126, 41),
+    ("Blue 1", 1, 121, 41),
+    ("Blue 2", 1, 122, 41),
+    ("Blue 3", 1, 123, 41),
+    ("Blue 4", 1, 124, 41),
+    ("Red", 1, 136, 42),
+    ("Red 1", 1, 131, 42),
+    ("Red 2", 1, 132, 42),
+    ("Jaguar 4", 1, 146, 42),
+    ("Harpy", 1, 331, 51),
+    ("Jaguar 5", 1, 156, 52),
+    ("Condor 1", 1, 311, 52),
+    ("Condor 2", 1, 312, 52),
+    ("Nighthawk", 1, 321, 52),
